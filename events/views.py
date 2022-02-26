2f8041c78d209a42fc2a403fcd1a16cf83e9b673
unknown <david.devine.111@gmail.com>
2022-02-26 14:46:55 +0000

    Event list upload

---
+++ events/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def details(request):
+    return render(request, 'events/details.html')
+
+def events(request):
+    return render(request, "events/events.html")
